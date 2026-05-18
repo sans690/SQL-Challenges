4b22c352c03342c04c3be722674e4025c1751768
sans690 <scsmith7255@gmail.com>
2026-05-18 17:50:19 -0400

    More qwq

--- Medium/Problems.sql
+++ Medium/Problems.sql
@@ -9,3 +9,11 @@ SELECT products.name, SUM(order_items.quantity) AS 'quantity' FROM products JOIN
 SELECT customers.first_name, customers.last_name FROM customers LEFT JOIN orders ON orders.customer_id = customers.id WHERE orders.id IS NULL;
 
 SELECT orders.id, orders.order_date FROM orders LEFT JOIN customers ON customers.id = orders.customer_id WHERE customers.id = 1 GROUP BY orders.order_date;
+
+SELECT DISTINCT customers.first_name, customers.last_name FROM customers JOIN orders ON orders.customer_id = customers.id JOIN order_items ON order_items.order_id = orders.id WHERE order_items.product_id = 2;
+
+SELECT SUM(quantity * price_per_unit) AS 'revenue' FROM orders JOIN order_items ON order_items.order_id = id WHERE order_date LIKE '%2023-03%';
+
+SELECT first_name, last_name FROM customers JOIN orders ON orders.customer_id = customers.id AND orders.status = 'Cancelled';
+
+SELECT customers.first_name, customers.last_name, order_count FROM customers JOIN (SELECT orders.customer_id, COUNT(*) AS 'order_count' FROM orders GROUP BY orders.customer_id) AS 'o' ON o.customer_id = customers.id WHERE order_count >= 2;
