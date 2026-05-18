c771c6dff74e51340cbd04b2cbde064544dafe49
sans690 <scsmith7255@gmail.com>
2026-05-17 20:10:34 -0400

    More problems done

--- Medium/Problems.sql
+++ Medium/Problems.sql
@@ -5,3 +5,7 @@ SELECT SUM(order_items.quantity * products.price) AS 'total_revenue' FROM produc
 SELECT customers.first_name, customers.last_name, order_count FROM customers LEFT JOIN (SELECT orders.customer_id, COUNT(*) AS 'order_count' FROM orders GROUP BY orders.customer_id) orders ON orders.customer_id = customers.id WHERE order_count > 2;
 
 SELECT products.name, SUM(order_items.quantity) AS 'quantity' FROM products JOIN order_items ON order_items.product_id = products.id WHERE order_items.order_id = 1 GROUP BY products.name;
+
+SELECT customers.first_name, customers.last_name FROM customers LEFT JOIN orders ON orders.customer_id = customers.id WHERE orders.id IS NULL;
+
+SELECT orders.id, orders.order_date FROM orders LEFT JOIN customers ON customers.id = orders.customer_id WHERE customers.id = 1 GROUP BY orders.order_date;
